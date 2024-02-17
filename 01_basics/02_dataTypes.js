2964068c6fa3a4acb80b680a45d4a01284fe00ba
Shubham Vishwakarma <153703088+ShubhamViswa@users.noreply.github.com>
2024-02-18 01:11:22 +0530

    Add files via upload

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3 + 3 ) // we are using nodejs , not browser 
+
+// console.log(3 
+//     + 
+//       3); // code readability should be high 
+
+// console.log("Shubham")
+
+
+let name = "Shubham "
+let age = 18 
+let isLoggedIn = false 
+let state; 
+
+
+ /**primitive Data types 
+number => 2 to power 53 
+bigint => 
+string => " "
+boolean => true / false 
+null => standalone value 
+undefined => 
+symbol => unique **/
+
+// object 
+
+console.log(typeof "Shubham ") // string 
+console.log(typeof age )  // number 
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined 
+
+
+
+
+
+
+/**Non - Primitive Data types 
+ * 
+ */
+
